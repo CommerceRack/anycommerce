0acbfe3bdf94be168b6fcdeb9af76487f3a127f7
JT Laugesen <jt@zoovy.com>
2012-11-19 16:36:36 -0800

    admin - tuning
    add meta headers so login landing page is not cached or spidered.
    add special error handling to login for 100 (system clock) error.

--- extensions/admin/extension.js
+++ extensions/admin/extension.js
@@ -449,7 +449,10 @@ else	{
 				app.ext.admin.u.showHeader();
 				},
 			onError : function(responseData){
-				app.u.throwMessage(responseData);	
+				app.u.throwMessage(responseData);
+				if(responseData.errid == "100")	{
+					app.u.throwMessage("This is most typically due to your system clock not being set correctly. For security, it must be set to both the correct time and timezone.");
+					} //this is the clock issue.
 				$('#preloadAndLoginContents').hideLoading();
 				}
 			}, //showHeader
