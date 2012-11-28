6e3010b52055d73e40167a4e023eb023b130b799
JT <jt@zoovy.com>
2012-11-27 19:32:39 -0800

    version model

--- model.js
+++ model.js
@@ -81,7 +81,7 @@ app.globalAjax.lastDispatch - keeps track of when the last dispatch occurs. Not 
 function zoovyModel() {
 	var r = {
 	
-		version : "201246",
+		version : "201248",
 	// --------------------------- GENERAL USE FUNCTIONS --------------------------- \\
 	
 	//pass in a json object and the last item id is returned.
