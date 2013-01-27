cecd5c59aca03ab60bdda96417312eb96e956add
JT <jt@zoovy.com>
2013-01-27 09:02:09 -0800

    admin - fix wrong logo issue

--- extensions/admin/extension.js
+++ extensions/admin/extension.js
@@ -1026,7 +1026,7 @@ if(uriParams.debug)	{
 	app.u.dump("Is anycommerce? document.domain: "+document.domain+" and uriParams.anycommerce: ["+uriParams.anycommerce+"]");
 	
 //the zoovy branding is in place by default. override if on anycommerce.com OR if an anycommerce URI param is present (for debugging)
-if((document.domain && document.domain.toLowerCase().indexOf('anycommerce')) || uriParams.anycommerce)	{
+if((document.domain && document.domain.toLowerCase().indexOf('anycommerce')) || app.u.isSet(uriParams.anycommerce))	{
 	$('.logo img').attr('src','extensions/admin/images/anycommerce_logo-173x30.png');
 	$('body').addClass('isAnyCommerce');
 	}
