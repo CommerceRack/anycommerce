1b64aee0fbc04dee0f9608396b388aadf43e4c8e
JT <jt@zoovy.com>
2013-02-05 11:55:09 -0800

    gts fixes

--- downlite_init.js
+++ downlite_init.js
@@ -15,8 +15,9 @@ app.rq.push(['extension',0,'store_cart','extensions/store_cart.js']);
 app.rq.push(['extension',0,'store_crm','extensions/store_crm.js']);
 app.rq.push(['extension',0,'myRIA','quickstart.js','startMyProgram']);
 
-app.rq.push(['extension',1,'analytics_google','extensions/analytics_google.js','startExtension']);
-app.rq.push(['extension',1,'google_ts','extensions/partner_google_trusted_store','startExtension']);
+app.rq.push(['extension',1,'analytics_google','extensions/analytics_google.js','addTriggers']);
+app.rq.push(['extension',1,'google_ts','extensions/partner_google_trusted_store.js','addTriggers']);
+
 //app.rq.push(['extension',1,'bonding_buysafe','extensions/bonding_buysafe.js','startExtension']);
 //app.rq.push(['extension',1,'powerReviews','extensions/reviews_powerreviews.js','startExtension']);
 //app.rq.push(['extension',0,'magicToolBox','extensions/imaging_magictoolbox.js','startExtension']); // (not working yet - ticket in to MTB)
