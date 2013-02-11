c317456be445c5b100df9a1a067816a8d2a2549a
Rebecca Sims <rebecca@rebecca-sims.com>
2013-02-11 15:06:34 -0800

    Anytabs.js update from JT

--- anytabs.js
+++ anytabs.js
@@ -80,11 +80,9 @@ or this: $('#bob').find('.ui-tabs-nav li:nth-child(2)').trigger('click');
 					self.reveal($(this));
 					});
 				});			
-			this.tabs.find('li a').each(function(){
-				$(this).click(function(event){
-					event.preventDefault();
-					});
-				});
+			this.tabs.find('li a').each($(this).on('click',function(event){
+			event.preventDefault();
+			}));
 			},
 
 			
