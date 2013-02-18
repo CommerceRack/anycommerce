4e0d5ddc97f2b9c6540d98e288110ff06b3df403
Rebecca S. <rebecca@rebecca-sims.com>
2013-02-18 14:36:50 -0800

    Commenting out old addthis code

--- downlite_init.js
+++ downlite_init.js
@@ -57,7 +57,7 @@ app.rq.push(['script',0,(document.location.protocol == 'https:' ? 'https:' : 'ht
 //for banner slideshow used on homepage.
 app.rq.push(['script',1,app.vars.baseURL+'cycle-2.9998.js']);//','validator':function(){return (jQuery().cycle) ? true : false;}});
 //renders AddThis block on product pages
-app.rq.push(['script',5,(document.location.protocol == 'https:' ? 'https:' : 'http:')+'//s7.addthis.com/js/250/addthis_widget.js#pubid=ra-4f26cd7d7f87f12', function(P) {var addthis_config = {"data_track_clickback":false,"ui_click":true,"ui_use_image_picker": true};}]);//	'validator':function(){return (typeof addthis == 'object') ? true : false;}
+//app.rq.push(['script',5,(document.location.protocol == 'https:' ? 'https:' : 'http:')+'//s7.addthis.com/js/250/addthis_widget.js#pubid=ra-4f26cd7d7f87f12', function(P) {var addthis_config = {"data_track_clickback":false,"ui_click":true,"ui_use_image_picker": true};}]);//	'validator':function(){return (typeof addthis == 'object') ? true : false;}
 
 
 
@@ -203,22 +203,22 @@ app.u.appInitComplete = function(P)	{
 //			app.renderFunctions.translateSelector('#hotelListContainer',app.data['appCategoryDetail|.hotels']);
 //			}
 //		});
-	//addthis code for productTemplate
-	app.rq.push(['templateFunction','productTemplate','onCompletes',function(P) {
-	var url = zGlobals.appSettings.http_app_url+"product/"+P.pid+"/";
+	//addthis code for productTemplate - replaced by partner_addthis extension
+//	app.rq.push(['templateFunction','productTemplate','onCompletes',function(P) {
+//	var url = zGlobals.appSettings.http_app_url+"product/"+P.pid+"/";
 	//update the openGraph and meta content. mostly for social/addThis.
-	$('#ogTitle').attr('content',app.data[P.datapointer]['%attribs']['zoovy:prod_name']);
-	$('#ogImage').attr('content',app.u.makeImage({"name":app.data[P.datapointer]['%attribs']['zoovy:prod_image1'],"w":150,"h":150,"b":"FFFFFF","tag":0}));
-	$('#ogDescription, #metaDescription').attr('content',app.data[P.datapointer]['%attribs']['zoovy:prod_desc']);
-			addthis.toolbox('#socialLinks');
-	if(typeof addthis == 'object' && addthis.update)	{
-		addthis.update('share','url',url);
-		$("#socialLinks .addthis_button_facebook_like").attr("fb:like:href",url);
-		$("#socialLinks .addthis_button_pinterest_pinit").attr({"pi:pinit:media":app.u.makeImage({"h":"300","w":"300","b":"ffffff","name":app.data['appProductGet|'+P.pid]['%attribs']['zoovy:prod_image1'],"tag":0}),"pi:pinit:url":url});	
-		}
+//	$('#ogTitle').attr('content',app.data[P.datapointer]['%attribs']['zoovy:prod_name']);
+//	$('#ogImage').attr('content',app.u.makeImage({"name":app.data[P.datapointer]['%attribs']['zoovy:prod_image1'],"w":150,"h":150,"b":"FFFFFF","tag":0}));
+//	$('#ogDescription, #metaDescription').attr('content',app.data[P.datapointer]['%attribs']['zoovy:prod_desc']);
+//			addthis.toolbox('#socialLinks');
+//	if(typeof addthis == 'object' && addthis.update)	{
+//		addthis.update('share','url',url);
+//		$("#socialLinks .addthis_button_facebook_like").attr("fb:like:href",url);
+//		$("#socialLinks .addthis_button_pinterest_pinit").attr({"pi:pinit:media":app.u.makeImage({"h":"300","w":"300","b":"ffffff","name":app.data['appProductGet|'+P.pid]['%attribs']['zoovy:prod_image1'],"tag":0}),"pi:pinit:url":url});	
+//		}
 
-		}]); //addThis productTemplate code		
-	}
+//		}]); //addThis productTemplate code		
+//	}
 
 
 //app.rq.push(['templateFunction','homepageTemplate','onCompletes',function(P) {
