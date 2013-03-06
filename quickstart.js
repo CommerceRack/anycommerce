7aa4b29fd12c10dbaaebeb8abde512ab90a13a3c
Michael Chance <michaelc@zoovy.com>
2013-03-06 10:03:13 -0800

    Wrapper styling, quickstart.js fix

--- quickstart.js
+++ quickstart.js
@@ -800,7 +800,7 @@ fallback is to just output the value.
 						buttonText = 'Choose Size'; className = 'variational sizeable';
 						}
 //pdata is a shortcut to attribs.
-					else if(data.value['@variations'].length)	{
+					else if(data.value['@variations'] && data.value['@variations'].length)	{
 						buttonText = 'Choose Options'; className = 'variational';
 						}
 					else	{
