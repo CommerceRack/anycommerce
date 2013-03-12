990060f47765c5aa07c1af3fd47559feb4564e38
Michael Chance <michaelc@zoovy.com>
2013-03-12 13:45:51 -0700

    Google Analytics Fix

--- downlite_init.js
+++ downlite_init.js
@@ -16,9 +16,9 @@ app.rq.push(['extension',0,'store_crm','extensions/store_crm.js']);
 app.rq.push(['extension',0,'myRIA','quickstart.js','startMyProgram']);
 app.rq.push(['extension',0,'partner_addthis','extensions/partner_addthis.js','startExtension']); //MC's addthis ext from 201307
 
-app.rq.push(['extension',1,'analytics_google','extensions/analytics_google.js','addTriggers']); //old default callback.
+app.rq.push(['extension',1,'analytics_google','extensions/analytics_google.js','startExtension']); //old default callback.
 app.rq.push(['extension',1,'google_ts','extensions/partner_google_trusted_store.js','startExtension']); //new default callback.
-//app.rq.push(['extension',1,'google_adwords','extensions/partner_google_adwords.js','startExtension']);
+app.rq.push(['extension',1,'google_adwords','extensions/partner_google_adwords.js','startExtension']);
 
 //app.rq.push(['extension',1,'bonding_buysafe','extensions/bonding_buysafe.js','startExtension']);
 //app.rq.push(['extension',1,'powerReviews','extensions/reviews_powerreviews.js','startExtension']);
