d5ac5efbf6cffa710a9dbc3d155f7dec5b0a97df
JT Laugesen <jt@zoovy.com>
2013-04-02 05:59:13 -0700

    1pc - some debugging

--- extensions/checkout_mobile/extension.js
+++ extensions/checkout_mobile/extension.js
@@ -54,9 +54,10 @@ var orderCreate = function() {
 //callbacks.init need to return either a true or a false, depending on whether or not the file will execute properly based on store account configuration.
 		init : {
 			onSuccess : function()	{
-//				app.u.dump('BEGIN app.ext.orderCreate.init.onSuccess');
+				app.u.dump('BEGIN app.ext.orderCreate.init.onSuccess');
 //1PC can't load the templates remotely. causes XSS issue.
 				if(app.vars._clientid == '1pc')	{
+					app.u.dump(" -> _cliendID = 1pc. load templates from TOXML file");
 					app.model.loadTemplates(theseTemplates); //loaded from local file (main.xml)
 					}
 				else {
