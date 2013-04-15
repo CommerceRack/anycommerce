129a89948df5ada136bde67d0ec04c4ce675c74b
JT Laugesen <jt@zoovy.com>
2013-04-15 08:25:35 -0700

    anyplugin - some debug for IE8 issue

--- resources/jquery.ui.anyplugins.js
+++ resources/jquery.ui.anyplugins.js
@@ -712,7 +712,7 @@ and it'll turn the cb into an ios-esque on/off switch.
 			else if(self.element.is(':checkbox'))	{$label = self.element.closest('label')}
 			else	{}
 			
-			app.u.dump("ANYCB -> 20130415");
+			app.u.dump("ANYCB -> 20130415b");
 			
 			if($label.data('anycb') === true)	{app.u.dump(" -> already anycb-ified");} //do nothing, already anycb-ified
 			else if($label.length)	{
@@ -720,7 +720,7 @@ and it'll turn the cb into an ios-esque on/off switch.
 				$container = $("<span \/>").addClass('ui-widget ui-widget-content ui-corner-all ui-widget-header').css({'position':'relative','display':'block','width':'55px','margin-right':'6px','height':'20px','z-index':1,'padding':0,'float':'left','cursor':'pointer'}),
 				$span = $("<span \/>").css({'padding':'0px','width':'30px','text-align':'center','height':'20px','line-height':'20px','position':'absolute','top':-1,'z-index':2,'font-size':'.75em','cursor':'pointer'});
 	
-				$label.data('anycb',true);
+				$label.data('anycb',true); //allows for plugin to check if it's already been run on this element.
 				self.span = $span; //global (within instance) for easy reference.
 
 				$label.contents().filter(function() {
@@ -729,6 +729,8 @@ and it'll turn the cb into an ios-esque on/off switch.
 				$input.hide();
 				$container.append($span);
 				$label.prepend($container);
+				
+				app.u.dump(" -> $input.length: "+$input.length);
 				$input.is(':checked') ? self._turnOn() : self._turnOff(); //set default
 				
 				$input.on('click.anycb',function(){
