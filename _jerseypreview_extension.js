c983dc67dbf9c0997c52fd6b53859208cef845dc
Michael Chance <michaelc@zoovy.com>
2013-05-06 21:14:19 -0700

    timeout scope fix

--- _jerseypreview_extension.js
+++ _jerseypreview_extension.js
@@ -87,7 +87,7 @@ var jerseypreview = function() {
 						app.ext.jerseypreview.u.thisMovie('jerseyPreview'+pid).goHome(name,number);
 						}
 					catch(err){
-						setTimeout(function(){this.setJerseyText(pid,name,number,attempts+1);},250);
+						setTimeout(function(){app.ext.jerseypreview.a.setJerseyText(pid,name,number,attempts+1);},250);
 						}
 					}
 				else {
