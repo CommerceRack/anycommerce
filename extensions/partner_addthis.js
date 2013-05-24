4c2674ee12088725253ca0ed4c3b876a0821f657
Michael Chance <michaelc@zoovy.com>
2013-05-24 15:52:34 -0700

    8 Addthis Buttons

--- extensions/partner_addthis.js
+++ extensions/partner_addthis.js
@@ -73,6 +73,10 @@ var partner_addthis = function() {
 								+		'<a class="addthis_button_preferred_2"></a>'
 								+		'<a class="addthis_button_preferred_3"></a>'
 								+		'<a class="addthis_button_preferred_4"></a>'
+								+		'<a class="addthis_button_preferred_5"></a>'
+								+		'<a class="addthis_button_preferred_6"></a>'
+								+		'<a class="addthis_button_preferred_7"></a>'
+								+		'<a class="addthis_button_preferred_8"></a>'
 								+		'<a class="addthis_button_compact"></a>'
 								+	'</div>');
 							
