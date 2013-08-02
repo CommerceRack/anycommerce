3188aa433adc0618c50df789c4707afb2f2dc9fb
JT <jt@zoovy.com>
2013-08-02 12:38:03 -0700

    admin - start of navcats file

---
+++ extensions/admin/navcats.js
@@ -0,0 +1,112 @@
+/* **************************************************************
+
+   Copyright 2013 Zoovy, Inc.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+************************************************************** */
+
+
+
+var admin_navcats = function() {
+	var theseTemplates = new Array('');
+	var r = {
+
+
+////////////////////////////////////   CALLBACKS    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+
+
+	callbacks : {
+//executed when extension is loaded. should include any validation that needs to occur.
+		init : {
+			onSuccess : function()	{
+				var r = false; //return false if extension won't load for some reason (account config, dependencies, etc).
+
+				//if there is any functionality required for this extension to load, put it here. such as a check for async google, the FB object, etc. return false if dependencies are not present. don't check for other extensions.
+				r = true;
+
+				return r;
+				},
+			onError : function()	{
+//errors will get reported for this callback as part of the extensions loading.  This is here for extra error handling purposes.
+//you may or may not need it.
+				app.u.dump('BEGIN admin_orders.callbacks.init.onError');
+				}
+			}
+		}, //callbacks
+
+
+
+////////////////////////////////////   ACTION    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+//actions are functions triggered by a user interaction, such as a click/tap.
+//these are going the way of the do do, in favor of app events. new extensions should have few (if any) actions.
+		a : {
+
+			}, //Actions
+
+////////////////////////////////////   RENDERFORMATS    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+//renderFormats are what is used to actually output data.
+//on a data-bind, format: is equal to a renderformat. extension: tells the rendering engine where to look for the renderFormat.
+//that way, two render formats named the same (but in different extensions) don't overwrite each other.
+		renderFormats : {
+
+			}, //renderFormats
+////////////////////////////////////   UTIL [u]   \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+//utilities are typically functions that are exected by an event or action.
+//any functions that are recycled should be here.
+		u : {
+			
+/*
+mode could be:  builder, selector
+*/
+			getTree : function(mode,vars){
+				var $tree = $("<div \/>").attr('data-app-role','categoryTree').data(vars).data('mode',mode);
+				vars = vars || {};
+				//set some defaults.
+				if(mode)	{
+					if(vars.root && vars.filter && vars.templateID)	{
+
+//everything necessary is here. proceed.
+app.ext.admin.calls.appCategoryList.init({'root':vars.root,'filter':vars.filter},{'callback':function(rd)	{
+	if(app.model.responseHasErrors(rd)){
+		$tree.anymessage({'message':rd});
+		}
+	else	{
+		//success content goes here.
+		}
+	}},'mutable');
+app.model.dispatchThis('mutable');
+
+						}
+					else	{
+						$tree.anymessage({'message':'In admin_navcats.u.getTree, root ['+vars.root+'] and/or filter ['+vars.filter+'] and/or filter ['+vars.templateID+'] not set or invalid.','gMessage':true});
+						}
+
+					}
+				else	{
+					$tree.anymessage({'message':'In admin_navcats.u.getTree, mode ['+mode+'] not set or invalid.','gMessage':true});
+					}
+				return $tree;
+				}
+			}, //u [utilities]
+
+		e : {
+			
+			} //e [app Events]
+		} //r object.
+	return r;
+	}
