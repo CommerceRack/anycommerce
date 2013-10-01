abe451af268971a3e04e33ff75c49d56b4a9143d
brianhorakh <brianh@zoovy.com>
2013-09-30 20:46:07 -0700

    updated video link

--- extensions/admin/extension.js
+++ extensions/admin/extension.js
@@ -71,7 +71,7 @@ var admin = function() {
 			// YOUTUBE RELEASE VIDEO:
 			versionMetaData : {
 				'youtubeVideoIDs' : {
-					'201338':'DJkZ70P3-1E',
+					'201338':'A8TNbpQtgas',
 					'201336':'UOfn6tiQqiw',
 					'201334':'FUO0NALw6sI',
 					'201332':'tKQ_SJzjbXI',
