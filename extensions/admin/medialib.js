354370d05fdce53c2a5f2d48ced604dbbc8ddbd2
JT <jt@zoovy.com>
2013-10-09 10:36:55 -0700

    media lib - bug fix
    There was a bug that caused the folder that was just uploaded into to
    not re-open after the upload.

--- extensions/admin/medialib.js
+++ extensions/admin/medialib.js
@@ -314,7 +314,10 @@ setTimeout(function(){
 			
 				$('#mediaLibControlsTabContainer').tabs();
 //in some cases, we may re-run this callback (such as after a file upload) and we need to open the folder on the left and in the media area opened for continuity.
-				if(app.ext.admin_medialib.u.getOpenFolderName())	{app.ext.admin_medialib.u.openMediaFolderByFilePath(app.ext.admin_medialib.u.getOpenFolderName())}
+				if(app.ext.admin_medialib.u.getOpenFolderName())	{
+//					app.u.dump(" -> app.ext.admin_medialib.u.getOpenFolderName(): "+app.ext.admin_medialib.u.getOpenFolderName());
+					app.ext.admin_medialib.u.openMediaFolderByFilePath(app.ext.admin_medialib.u.getOpenFolderName())
+					}
 //for whatever reason, jqfu has decided it doesn't want to init properly right away. a slight pause and it works fine. weird. ### need a better long term solution.
 				setTimeout(function(){
 					app.ext.admin_medialib.u.convertFormToJQFU('#mediaLibUploadForm','mediaLibrary'); //turns the file upload area into a jquery file upload
@@ -794,7 +797,8 @@ if(selector && mode)	{
 //	app.u.dump(" -> $selector: "); app.u.dump($selector);
 	var successCallbacks = {
 
-	//The dispatches in this request are immutable. the imageUpload and updates need to happen at the same time to provide a good UX and the image creation should be immutable.
+//The dispatches in this request are immutable. the imageUpload and updates need to happen at the same time to provide a good UX and the image creation should be immutable.
+//This code could get executed several times during a large batch of files. Any code needed for 1 time execution (at the end) should be in the fileuploadstopped function.
 		'mediaLibrary' : function(data,textStatus){
 			var L = data.length;
 			var tagObj;
@@ -1023,19 +1027,19 @@ else	{
 //the root LI's contain UL's with their FID in the ID. (mediaChildren_FID) (arguably, should have been mediaChildrenOf_ to indicate better).
 //each of these UL's contain all the properties of the parent folder. fid, fname, etc
 			openMediaFolderByFilePath : function(path)	{
-//				app.u.dump("BEGIN admin_medialib.u.openMediaFolderByFilePath ["+path+"]");
+				app.u.dump("BEGIN admin_medialib.u.openMediaFolderByFilePath ["+path+"]");
 //if no slashes or periods, is a root category.
 				if(path && path.indexOf('/') == -1 && path.indexOf('.') == -1){
-//					app.u.dump(" -> is a root folder.");
-					$('#mediaRootFolder_'+path+' a:first').click();
+					$("li[data-fname='"+path+"']:first",'#mediaLibFolderListUL').find('a:first').trigger('click');
 					}
 				else if(path)	{
-
+					app.u.dump(" -> is a sub folder");
 					var pathArray = path.split('/');
 					var path2Now = pathArray[0]; //puts path back together again. each pass it adds a folder back, starting with the root and working down 2 the last.
 					var L = (path.indexOf('.') > -1) ? pathArray.length - 1 : pathArray.length; //if last spot is filename, ignore.
 //					app.u.dump(" -> L: "+L);
-					var $rootCat = $('#mediaRootFolder_'+pathArray[0])
+					var $rootCat = $("li[data-fname='"+pathArray[0]+"']:first",'#mediaLibFolderListUL'); //$('#mediaRootFolder_'+pathArray[0])
+					app.u.dump(" -> $rootCat.length: "+$rootCat.length);
 					var fid = $rootCat.data('fid'); //root folder has fname in the id, but all properties in data.
 					var $tmp;
 					$('#mediaChildren_'+fid).toggle(); //turn first set of subfolders.
