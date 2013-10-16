0ce585a11ca2551ba6d4f747cbbfa13c90f9ce5f
JT <jt@zoovy.com>
2013-10-16 07:29:28 -0700

    checkout - undo some IE specific changes
    determined the IE issue w/ 1PC is related to an inline block style used
    on jquery UI buttons.

--- extensions/checkout/extension.js
+++ extensions/checkout/extension.js
@@ -1178,8 +1178,7 @@ note - the order object is available at app.data['order|'+P.orderID]
 				}, //execBuyerLogin
 
 			execCartOrderCreate : function($btn)	{
-// ### disabled to test an IE bug causing the button to load in the wrong part of the page.
-//				$btn.addClass('ui-state-highlight').button();
+				$btn.addClass('ui-state-highlight').button();
 
 				$btn.off('click.execCartOrderCreate').on('click.execCartOrderCreate',function(event){
 					event.preventDefault();
