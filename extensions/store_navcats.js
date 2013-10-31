0c71390662d2fca3ffca578fe58109260b2a0c58
MichaelC <michaelc@anycommerce.com>
2013-10-31 12:37:33 -0700

    store_navcats - null pretty name fix in categoryList

--- extensions/store_navcats.js
+++ extensions/store_navcats.js
@@ -419,7 +419,8 @@ templateID - the template id used (from app.templates)
 							app.u.dump("WARNING! invalid value for 'detail' in categoryList renderFunction");
 							}
 						for(var i = 0; i < L; i += 1)	{
-							if(data.value[i].pretty[0] != '!')	{
+// *** 201344 null pretty name is NOT a hidden category. But we have to check to avoid a null ptr error. -mc
+							if(!data.value[i].pretty || data.value[i].pretty[0] != '!')	{
 // ** 201336+ appNavcatDetail id param changed to path -mc
 // *** 201338 Missed a few references to id here -mc
 								var parentID = data.value[i].path+"_catgid+"+(app.u.guidGenerator().substring(10));
