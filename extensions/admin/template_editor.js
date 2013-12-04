dcd68c694f8150a1a7cf96dea072710b6f60cce6
JT <jt@zoovy.com>
2013-12-04 08:50:16 -0800

    template manager - remove reference to 'createFrom'
    replaced with copyFrom

--- extensions/admin/template_editor.js
+++ extensions/admin/template_editor.js
@@ -1425,7 +1425,7 @@ var $input = $(app.u.jqSelector('#',ID));
 								}
 
 							// we should fix this naming: .. CopyFrom makes the most sense to me. -bh
-							dObj._cmd = (mode == 'EBAYProfile') ? 'adminEBAYTemplateCreateFrom' : 'admin'+mode+'TemplateCopyFrom';
+							dObj._cmd = (mode == 'EBAYProfile') ? 'adminEBAYTemplateCopyFrom' : 'admin'+mode+'TemplateCopyFrom';
 
 							if(mode == 'EBAYProfile')	{dObj.PROFILE = $D.data('profile');}
 							else if(mode == 'Campaign')	{dObj.CAMPAIGNID = $D.data('campaignid');}
