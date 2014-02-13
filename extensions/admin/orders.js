a0e4d82411b26d15518100b26f34b3693c0d0551
jt-zoovy <jt@zoovy.com>
2014-02-13 09:40:15 -0800

    orders - revert print test
    The test worked on several platforms/browsers, but not on android/chrome
    on the phone we tested on.

--- extensions/admin/orders.js
+++ extensions/admin/orders.js
@@ -2007,12 +2007,12 @@ $('.editable',$container).each(function(){
 				if(orderID && $ele.data('loadstemplate'))	{
 // SANITY -> browsers didn't like it when the popup was triggered after an ajax request. so instead, we open the popup right away, then populate the content later, followed by the print cmd.
 
-// A new approach to printing.  needs testing.
-var $iframe = $("<iframe \/>").attr({'id':'printContainerIframe','name':'printContainerIframe'}).appendTo($("#printContainer").empty()); //the print container.  emptied to make sure anything leftover from last print is gone.
-$iframe.contents().find('body').append('<h1>JT WAS HERE!</h1>')
-$iframe.contents().find('head').append('<style>@media print{.pageBreak {page-break-after:always} .hide4Print {display:none;}}</style>');
-window.frames["printContainerIframe"].focus(); window.frames["printContainerIframe"].print();
-/*
+// ### FUTURE -> the iframe code below is a new approach to printing.  needs testing. -> worked on PC chrome and firefox. Worked on iphone safari. did NOT work on android chrome for ericH. that needs more testing.
+//var $iframe = $("<iframe \/>").attr({'id':'printContainerIframe','name':'printContainerIframe'}).appendTo($("#printContainer").empty()); //the print container.  emptied to make sure anything leftover from last print is gone.
+//$iframe.contents().find('body').append('<h1>JT WAS HERE!</h1>')
+//$iframe.contents().find('head').append('<style>@media print{.pageBreak {page-break-after:always} .hide4Print {display:none;}}</style>');
+//window.frames["printContainerIframe"].focus(); window.frames["printContainerIframe"].print();
+
 					var popup = window.open('','','left=0,top=0,width=600,height=600,toolbar=0,scrollbars=0,status=0');
 					if(popup)	{
 						popup.document.write("<style>@media print{.pageBreak {page-break-after:always} .hide4Print {display:none;}}</style><body style='font-family:sans-serif;'>Loading order content...</body></html>");
@@ -2051,7 +2051,7 @@ window.frames["printContainerIframe"].focus(); window.frames["printContainerIfra
 					else	{
 						//unable to open a popup.
 						}
-*/
+
 					}
 				else	{
 					$('#globalMessaging').anymessage({"message":"In admin_orders.e.orderPrint, either orderid ["+orderID+"] was unable to be ascertained or data-loadstemplate ["+$ele.data('loadstemplate')+"] not set on trigger element.","gMessage":true});
