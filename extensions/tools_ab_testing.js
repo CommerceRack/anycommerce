e05dbbafc6da865c2c3c838ae38ad63fef572fa5
Kim Horakh <kimh@zoovy.com>
2014-02-14 12:25:42 -0800

    Changes to ab testing

--- extensions/tools_ab_testing.js
+++ extensions/tools_ab_testing.js
@@ -32,7 +32,7 @@ Add support for data-mining- hooks for google analytics tracking
 */
 
 
-var tools_ABtesting = function() {
+var tools_ab_testing = function() {
 	var theseTemplates = new Array('');
 	var r = {
 
