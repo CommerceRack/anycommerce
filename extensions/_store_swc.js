e6d5b28dd49508a440a36e71bc62d1c10979683e
MichaelC <michaelc@zoovy.com>
2014-04-08 23:05:34 -0700

    static page handling update

--- extensions/_store_swc.js
+++ extensions/_store_swc.js
@@ -58,7 +58,9 @@ var store_swc = function(_app) {
 					$('#globalMessaging').anymessage({'message' : "It looks like this is your first time here!  We've added the Chicago Cubs to your Team list- to add or remove teams go <a href='#!customer/myteams/'>here!</a>", timeout:30000});
 					}
 				
-				_app.router.appendHash({'type':'exact','route':'fieldcam/','callback':function(routeObj){showContent('category',{'navcat':'.wrigley_field_cam', 'templateID':'fieldcamTemplate'})}});
+				_app.router.appendHash({'type':'exact','route':'fieldcam/','callback':function(routeObj){
+					showContent('static',{dataset:_app.ext.store_swc.staticData.fieldcam, 'templateID':'fieldcamTemplate'})
+					}});
 				_app.router.appendHash({'type':'match','route':'filter/{{id}}*','callback':function(routeObj){
 					if(_app.ext.store_swc.filterData[routeObj.params.id]){
 						routeObj.params.templateID = "filteredSearchTemplate";
@@ -93,6 +95,9 @@ var store_swc = function(_app) {
 				});
 				_app.templates.filteredSearchTemplate.on('complete.swc', function(event, $context, infoObj){
 					$('form.filterList', $context).trigger('submit');
+					});
+				_app.templates.fieldcamTemplate.on('depart.swc', function(event, $context, infoObj){
+					$context.empty().remove();
 					});
 				},
 			onError : function(){}
@@ -370,6 +375,12 @@ var store_swc = function(_app) {
 			'app_mlb' : [{"p":"Arizona Diamondbacks","v":"arizona_diamondbacks"},{"p":"Atlanta Braves","v":"atlanta_braves"},{"p":"Baltimore Orioles","v":"baltimore_orioles"},{"p":"Boston Red Sox","v":"boston_red_sox"},{"p":"Chicago Cubs","v":"chicago_cubs"},{"p":"Chicago White Sox","v":"chicago_white_sox"},{"p":"Cincinnati Reds","v":"cincinnati_reds"},{"p":"Cleveland Indians","v":"cleveland_indians"},{"p":"Colorado Rockies","v":"colorado_rockies"},{"p":"Detroit Tigers","v":"detroit_tigers"},{"p":"Houston Astros","v":"houston_astros"},{"p":"Kansas City Royals","v":"kansas_city_royals"},{"p":"L.A. Angels of Anaheim","v":"la_angels_of_anaheim"},{"p":"Los Angeles Dodgers","v":"los_angeles_dodgers"},{"p":"Miami Marlins","v":"miami_marlins"},{"p":"Milwaukee Brewers","v":"milwaukee_brewers"},{"p":"Minnesota Twins","v":"minnesota_twins"},{"p":"New York Mets","v":"new_york_mets"},{"p":"New York Yankees","v":"new_york_yankees"},{"p":"Oakland Athletics","v":"oakland_athletics"},{"p":"Philadelphia Phillies","v":"philadelphia_phillies"},{"p":"Pittsburgh Pirates","v":"pittsburgh_pirates"},{"p":"San Diego Padres","v":"san_diego_padres"},{"p":"San Francisco Giants","v":"san_francisco_giants"},{"p":"Seattle Mariners","v":"seattle_mariners"},{"p":"St. Louis Cardinals","v":"st_louis_cardinals"},{"p":"Tampa Bay Rays","v":"tampa_bay_Rays"},{"p":"Texas Rangers","v":"texas_rangers"},{"p":"Toronto Blue Jays","v":"toronto_blue_jays"},{"p":"Washington Nationals","v":"washington_nationals"},{"p":"EMPTY","v":""}],
 			'app_nfl' : [{"p":"Arizona Cardinals","v":"arizona_cardinals"},{"p":"Atlanta Falcons","v":"atlanta_falcons"},{"p":"Baltimore Ravens","v":"baltimore_ravens"},{"p":"Buffalo Bills","v":"buffalo_bills"},{"p":"Carolina Panthers","v":"carolina_panthers"},{"p":"Chicago Bears","v":"chicago_bears"},{"p":"Cincinnati Bengals","v":"cincinnati_bengals"},{"p":"Cleveland Browns","v":"cleveland_browns"},{"p":"Dallas Cowboys","v":"dallas_cowboys"},{"p":"Denver Broncos","v":"denver_broncos"},{"p":"Detroit Lions","v":"detroit_lions"},{"p":"Green Bay Packers","v":"green_bay_packers"},{"p":"Houston Texans","v":"houston_texans"},{"p":"Indianapolis Colts","v":"indianapolis_colts"},{"p":"Jacksonville Jaguars","v":"jacksonville_jaguars"},{"p":"Kansas City Chiefs","v":"kansas_city_chiefs"},{"p":"Miami Dolphins","v":"miami_dolphins"},{"p":"Minnesota Vikings","v":"minnesota_vikings"},{"p":"New England Patriots","v":"new_england_patriots"},{"p":"New York Giants","v":"new_york_giants"},{"p":"New Orleans Saints","v":"new_orleans_saints"},{"p":"New York Jets","v":"new_york_jets"},{"p":"Oakland Raiders","v":"oakland_raiders"},{"p":"Philadelphia Eagles","v":"philadelphia_eagles"},{"p":"Pittsburgh Steelers","v":"pittsburgh_steelers"},{"p":"San Diego Chargers","v":"san_diego_chargers"},{"p":"San Francisco 49ers","v":"san_francisco_49ers"},{"p":"Seattle Seahawks","v":"seattle_seahawks"},{"p":"St. Louis Rams","v":"st_louis_rams"},{"p":"Tampa Bay Buccaneers","v":"tampa_bay_buccaneers"},{"p":"Tennessee Titans","v":"tennessee_titans"},{"p":"Washington Redskins","v":"washington_redskins"},{"p":"EMPTY","v":""}],
 			'app_nhl' : [{"p":"Chicago Blackhawks","v":"chicago_blackhawks"},{"p":"EMPTY","v":""}],
+			},
+		staticData : {
+			"fieldcam" : {
+				"cam1" : '<iframe width="650" scrolling="no" height="366" frameborder="0" src="http://www.earthcam.com/js/cubworld.php" marginwidth="0" marginheight="0"></iframe>',
+				"cam2" : '<object width="650" height="480" align="middle" id="metro_cam_player_01" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,0,0" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"><param value="sameDomain" name="allowScriptAccess"><param value="http://www.earthcam.com/swf/dotcom_live_viewer_multi_size.swf?http://images.earthcam.com/ec_metros/ourcams/rosensports.jpg,50,1000" name="movie"><param value="high" name="quality"><param value="#000000" name="bgcolor"><embed width="650" height="480" align="middle" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" allowscriptaccess="sameDomain" name="metro_cam_player_01" bgcolor="#000000" quality="high" src="http://www.earthcam.com/swf/dotcom_live_viewer_multi_size.swf?http://images.earthcam.com/ec_metros/ourcams/rosensports.jpg,50,1000"></object>'
+				}
 			}
 		} //r object.
 	return r;
