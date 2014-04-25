956d4787847d9f8154f38b6fda676e1580633d91
Brad Rosen <Brosen@sportsworldchicago.com>
2014-04-25 10:19:30 -0500

    Cap Fix Again

--- extensions/_store_swc.js
+++ extensions/_store_swc.js
@@ -693,8 +693,8 @@ var store_swc = function(_app) {
 		validTeams : {
 			//These values taken from flex field setup, and should be adjusted when / if these are expanded
 			'app_nba' : [{"p":"Chicago Bulls","v":"chicago_bulls"}],
-			'app_mlb' : [{"p":"Arizona Diamondbacks","v":"arizona_diamondbacks", "img":"images/homepage/Arizona_Diamondbacks_Game_Cap.jpg"},
-						{"p":"Atlanta Braves","v":"atlanta_braves", "img":"images/homepage/Atlanta_Braves_Home_Cap.jpg"},
+			'app_mlb' : [{"p":"Arizona Diamondbacks","v":"arizona_diamondbacks", "img":"mlbhats/arizona_diamondbacks_game_47_franchise_cap6.jpg"},
+						{"p":"Atlanta Braves","v":"atlanta_braves", "img":"mlbhats/atlanta_braves_home_cap.jpg"},
 						{"p":"Baltimore Orioles","v":"baltimore_orioles", "img":"mlbhats/baltimore_orioles_alternate_47_franchise_cap6.png"},
 						{"p":"Boston Red Sox","v":"boston_red_sox", "img":"mlbhats/boston_red_sox_game_47_franchise_cap6.png"},
 						{"p":"Chicago Cubs","v":"chicago_cubs", "img":"47brand/chicago_cubs_royal_franchise_cap_by__47_brand.jpg"},
