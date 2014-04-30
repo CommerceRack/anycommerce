da87fa629d982f8185b62eaf1cad5cf43178e86b
Michael <michaelc@zoovy.com>
2014-04-29 19:19:37 -0700

    Addthis style update

--- extensions/partner_addthis.js
+++ extensions/partner_addthis.js
@@ -68,11 +68,15 @@ var partner_addthis = function(_app) {
 							}
 						else {
 							$toolbox.addClass('addThisRendered').append(
-									'<div class="addthis_toolbox addthis_default_style">'
+									'<div class="addthis_toolbox addthis_default_style addthis_32x32_style">'
 								+		'<a class="addthis_button_preferred_1"></a>'
 								+		'<a class="addthis_button_preferred_2"></a>'
 								+		'<a class="addthis_button_preferred_3"></a>'
 								+		'<a class="addthis_button_preferred_4"></a>'
+								+		'<a class="addthis_button_preferred_5"></a>'
+								+		'<a class="addthis_button_preferred_6"></a>'
+								+		'<a class="addthis_button_preferred_7"></a>'
+								+		'<a class="addthis_button_preferred_8add"></a>'
 								+		'<a class="addthis_button_compact"></a>'
 								+	'</div>');
 							
