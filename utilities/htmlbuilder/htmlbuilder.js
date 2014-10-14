f6da613293085d430094486d43e094ea059774c0
Michael <michaelc@zoovy.com>
2014-10-13 17:04:55 -0700

    htmlbuilder update

--- utilities/htmlbuilder/htmlbuilder.js
+++ utilities/htmlbuilder/htmlbuilder.js
@@ -74,88 +74,117 @@ var urls = JSON.parse(request.responseText);
 var today = new Date();
 var datestr = dateFormat(now,"yyyy-mm-dd");
 
-for( var i in urls['@OBJECTS'] ) {
-        // { id: '.mlb.boston_red_sox.z_david_ortiz', type: 'navcat' }
-        var res = urls['@OBJECTS'][i];
-		if(!res['seo:noindex']){
-				var url = '';
-				switch (res.type) {
-						case 'pid':
-								url = '/product/' + res.id;
-								break;
-						case 'navcat':
-								url = '/category/' + res.id.substr(1);  // strip leading . in category name
-								break;
-						case 'list' :
-								// we don't index these.
-								break;
-						default :
-								console.log(res);
-								break;
-						}
-				if (url) {
-						URLS.push(url);
-						}
-				}
-		else {
-			console.log('Following object was skipped due to inclusion of seo:noindex attribute:');
-			console.dir(res);
-				}
-        }
-
-var myConsole = console;	
+// for( var i in urls['@OBJECTS'] ) {
+        // // { id: '.mlb.boston_red_sox.z_david_ortiz', type: 'navcat' }
+        // var res = urls['@OBJECTS'][i];
+		// if(!res['seo:noindex']){
+				// var url = '';
+				// switch (res.type) {
+						// case 'pid':
+								// url = '/product/' + res.id;
+								// break;
+						// case 'navcat':
+								// url = '/category/' + res.id.substr(1);  // strip leading . in category name
+								// break;
+						// case 'list' :
+								// // we don't index these.
+								// break;
+						// default :
+								// console.log(res);
+								// break;
+						// }
+				// if (url) {
+						// URLS.push(url);
+						// }
+				// }
+		// else {
+			// console.log('Following object was skipped due to inclusion of seo:noindex attribute:');
+			// console.dir(res);
+				// }
+        // }
 		
-
+var doneCount = 0;
 		
 for(var i =0; i < opts['threads']; i++){
 	console.log('creating phantom');
 	phantom.create(function(err,ph){
 		return ph.createPage(function(err,page){
-			return page.open("http://"+DOMAIN+"/", (function(page){return function(err, status){
+			// page.onConsoleMessage = function(msg){
+				// console.log(msg);
+				// }
+			return page.open("http://"+DOMAIN+"/?_v="+new Date().getTime(), (function(page){return function(err, status){
 				console.log("opened site? "+status);
 				//HERE IS WHERE THE COOL SHIT HAPPENS
 				function hello(){
-					_robots.hello();
+					_robots.hello('phantom');
 					_robots.next();
 					}
 				function getStatus(){
-					return {
-						'status':_robots.status(), 
-						'html':document.innerHTML
-						};
+					var r = {
+						'status' : window._robots.status(),
+						'html': window.document.documentElement.outerHTML
+						}
+					if(window.__page){
+						r.page = window.__page;
+						}
+					return r
 					}
 				function getNextPage(){
 					var url = URLS.shift();
-					_robots.next(url);
+					return "function(){window.__page = '"+url+"';window._robots.next("+url+");}"
 					}
 				function handleContinue(err, result){
-					console.log(result);
+					//console.log(result);
 					if(result.status == 100){
 						setTimeout(function(){page.evaluate(getStatus, handleContinue)},100);
 						}
 					else {
 						console.log("Requesting next page");
 						console.log(URLS[0]);
-						page.evaluate(getNextPage)
+						page.evaluate(getNextPage())
 						page.evaluate(getStatus, handlePage);
 						}
 					}
 				function handlePage(err, result){
-					console.log(result);
+					//console.log(result);
 					if(result.status == 100){
 						setTimeout(function(){page.evaluate(getStatus, handlePage)},100);
 						}
 					else if (result.status == 200){
-						console.log(result.html);
+						if(result.page){
+							console.log('status 200 on page: '+result.page);
+							var filename = "./built/"+result.page.replace(/\//g,'')+".html";
+							console.log(filename);
+							fs.writeFileSync(filename, result.html);
+							}
+						evaluateNext();
 						}
 					else {
 						console.log(404);
+						evaluateNext();
+						}
+					
+					}
+				function evaluateNext(){
+					console.log(URLS.length);
+					if(URLS.length){
+						page.evaluate(getNextPage())
+						page.evaluate(getStatus, handlePage);
+						
+						}
+					else {
+						doneCount++;
+						if(doneCount >= opts['threads']){
+							process.exit(0);
+							}
 						}
 					}
-				page.evaluate(hello);
-				page.evaluate(getStatus, handleContinue);
+				setTimeout(function(){
+					page.evaluate(hello);
+					page.evaluate(getStatus, handleContinue);
+					}, 2000);
 				}})(page));
 			})
-		})
+		},{parameters : {'load-images':false}})
 	}
 
