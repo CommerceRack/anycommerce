2cc802857f5e28506751a5c6d0cb4d48345c42b5
Brad Rosen <brosen@sportsworldchicago.com>
2015-10-02 10:00:00 -0500

    Filter Update

--- extensions/_store_swc.js
+++ extensions/_store_swc.js
@@ -1845,7 +1845,7 @@ var store_swc = function(_app) {
 								{"id":"souvenirs","img":"game_time/Boston_Red_Sox_B_Logo_Womens_All_Star_Series_Watch"}
 							]
 						},
-						{"p":"Chicago Cubs","v":"chicago_cubs", "img":"47brand/chicago_cubs_royal_franchise_cap_by__47_brand.jpg", "catlink":"/category/.mlb.chicago_cubs/Chicago%20Cubs", "players" : ["Anthony Rizzo","Kris Bryant","Jake Arrieta","Kyle Schwarber"], 
+						{"p":"Chicago Cubs","v":"chicago_cubs", "img":"47brand/chicago_cubs_royal_franchise_cap_by__47_brand.jpg", "catlink":"/category/.mlb.chicago_cubs/Chicago%20Cubs", "players" : ["Anthony Rizzo","Kris Bryant","Jake Arrieta"], 
 							"filters" : [
 								{"id":"shirts","img":"5/chicago_cubs_blue_logo_t_shirt5"},
 								{"id":"jerseys","img":"Chicago_Cubs/Chicago_Cubs_Home_Authentic_Jersey"},
